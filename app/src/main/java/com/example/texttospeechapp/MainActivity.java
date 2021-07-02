39f5e12db4587a6e0c97752c3b9d34344464fa25
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-03 00:02:34 +0530

    call the setOnClickListener() method along with the object button

--- app/src/main/java/com/example/texttospeechapp/MainActivity.java
+++ app/src/main/java/com/example/texttospeechapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.OnInitListener;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -22,11 +23,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         button = (Button)findViewById(R.id.button);
         editText = (EditText)findViewById(R.id.editText);
+
         textToSpeech = new TextToSpeech(this, new OnInitListener(){
             @Override
             public void onInit(int i) {
                 textToSpeech.setLanguage(Locale.ENGLISH);
                 textToSpeech.setSpeechRate(0.5f);
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
     }
