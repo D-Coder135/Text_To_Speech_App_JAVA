a415aa1284fe5e70d9a66bf3be8ae9c1c87732bb
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-02 23:53:16 +0530

    define the object for the TextToSpeech class

--- app/src/main/java/com/example/texttospeechapp/MainActivity.java
+++ app/src/main/java/com/example/texttospeechapp/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.texttospeechapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     EditText editText;
     Button button;
+    TextToSpeech textToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
