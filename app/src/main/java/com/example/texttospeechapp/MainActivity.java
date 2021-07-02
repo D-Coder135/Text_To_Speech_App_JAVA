59889a428bb5c2847818d1c65696a86d7930120a
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-02 23:51:18 +0530

    define the object of the EditText class

--- app/src/main/java/com/example/texttospeechapp/MainActivity.java
+++ app/src/main/java/com/example/texttospeechapp/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.texttospeechapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
