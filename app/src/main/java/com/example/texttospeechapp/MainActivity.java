3a7226335e95f11f105270f59fb15c59f859ad94
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-03 00:03:32 +0530

    initialize a field of type String with the method call of the method getText() followed by the toString() method along with the object editText

--- app/src/main/java/com/example/texttospeechapp/MainActivity.java
+++ app/src/main/java/com/example/texttospeechapp/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                String textInEditText = editText.getText().toString();
             }
         });
     }
